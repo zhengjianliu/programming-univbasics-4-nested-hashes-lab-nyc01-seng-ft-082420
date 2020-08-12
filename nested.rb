ea2f1a886ec0df778c8fbdc755b554f8c24cd1ce
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 14:52:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
     return programmer_hash
 end
