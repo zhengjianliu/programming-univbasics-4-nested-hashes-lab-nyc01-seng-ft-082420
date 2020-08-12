a381f675c1b5e3c4d9b359817e0df89b11f33b23
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 14:53:03 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    return programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    return programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,7 +83,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = [{:known_for => "Ruby"}, {:languages => ["LISP", "C"]}]
+    return programmer_hash
 end
 
 def changing_alan
@@ -103,7 +106,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +129,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    return programmer_hash
 end
