66219e65667a2efd9b9ed7b23f884a03c3851af7
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 14:42:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    return programmer_hash[]
 
 end
 
