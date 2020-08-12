6e5dc134d6c00f14dc5d47b2364503f25def9e65
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 14:49:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
       }
     }
     programmer_hash[:yukihiro_matsumoto] = [{:known_for => "Ruby"}, {:languages => ["LISP", "C"]}]
-
+    return programmer_hash
 end
 
 def changing_alan
